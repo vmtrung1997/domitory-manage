65123c1b5f555c68a20acac6cc4419d3da215125
tranthanhtrung <tranthanhtrung1239@gmail.com>
2019-02-20 15:39:20 +0700

    create Avatar component

--- src/frontend/src/app/App.js
+++ src/frontend/src/app/App.js
@@ -3,13 +3,12 @@ import { createStore } from 'redux'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import './App.css'
-import Button from './components/button/button'
 
 class App extends Component {
   render() {
     return (
     	<div>
-     		heelo
+    		hello
      	</div>
     );
   }
