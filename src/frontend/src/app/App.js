1286e62bc67218fc2ee94580a7b38cb10f306147
tranthanhtrung <tranthanhtrung1239@gmail.com>
2019-02-20 13:22:10 +0700

    get event click on button component

--- src/frontend/src/app/App.js
+++ src/frontend/src/app/App.js
@@ -9,12 +9,7 @@ class App extends Component {
   render() {
     return (
     	<div>
-     		<Button type={"bt-outline bt-primary"} content={"Đồng ý"} size="bt-lg" getValue={this.getClick}/>
-     		<Button type={"bt-outline bt-warning"} content={"123"} size="bt-sm"/>
-     		<Button type={"bt-outline bt-danger bt-circle"} content={"123"} size="bt-xs"/>
-     		<Button type={"bt-info bt-circle"} firstIcon={"fas fa-check"}/>
-     		<Button type={"bt-default bt-circle"} content={"123"} height={100} width={100}/>
-     		<Button type={"bt-success"} content={"123"}/>
+     		heelo
      	</div>
     );
   }
--- src/frontend/src/app/components/button/button.js
+++ src/frontend/src/app/components/button/button.js
@@ -5,8 +5,9 @@ class Button extends Component{
 	render(){
 		const value = this.props.content
 		return(
-			<button  style={{margin: "4px", height: `${this.props.height}px`, width: `${this.props.width}px`}}
+			<button  style={{margin: "4px", height: `${this.props.height}px`, width: `${this.props.width}px`, borderRadius: `${this.props.radius}px`}}
 				className={"bt " + this.props.type + " " + this.props.size}
+				onClick={this.props.onClick}
 			>
 				<i className={this.props.firstIcon}></i>
 				{value}
@@ -17,3 +18,19 @@ class Button extends Component{
 }
 
 export default Button;
+
+// handle = () => {
+// 		console.log(1);
+// 	}
+//   render() {
+//     return (
+//     	<div>
+//      		<Button type={"bt-outline bt-primary"} content={"Đồng ý"} size="bt-lg" getValue={this.getClick}/>
+//      		<Button type={"bt-outline bt-warning"} content={"123"} size="bt-sm"/>
+//      		<Button type={"bt-outline bt-danger bt-circle"} content={"123"} size="bt-xs"/>
+//      		<Button type={"bt-info bt-circle"} firstIcon={"fas fa-check"}/>
+//      		<Button type={"bt-default bt-circle"} content={"123"} height={100} width={100}/>
+//      		<Button type={"bt-success"} content={"123"} radius={10} onClick={this.handle}/>
+//      	</div>
+//     );
+//   }
