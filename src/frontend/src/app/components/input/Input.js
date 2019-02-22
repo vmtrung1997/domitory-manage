6ee0baf77b86dfdc52250bed24ff23d771d62a23
MINH-TRUNG-VO\Admin <vmtrung1997@gmail.com>
2019-02-22 16:09:19 +0700

    edi selectOption radioButton input checkbox - add modal datepicker

--- src/frontend/src/app/components/input/Input.js
+++ src/frontend/src/app/components/input/Input.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Input.css'
 
 class Input extends Component {
+  static propTypes = {
+    defaultValue: PropTypes.string,
+    placeholder: PropTypes.string
+  }
+  static defaultProps = {
+    type: 'text',
+    width: '100%',
+    fontSize: '14px',
+    placeholder: '',
+    getValue: () => {}
+  }
   onChange = (event) => {
     this.props.getValue(event.target.value);
   }
   render() {
-    const placeholder = this.props.placeholder
-    const type = this.props.type
     return (
       <React.Fragment>
-        <input onChange={this.onChange} defaultValue={this.props.defaultValue} type={type} className="input-control" placeholder={placeholder}/>
+        <input 
+          style={{width: `${this.props.width}`, fontSize: `${this.props.fontSize}`}}
+          onChange={this.onChange} 
+          defaultValue={this.props.defaultValue} 
+          type={this.props.type} 
+          className="input-control" 
+          placeholder={this.props.placeholder} />
       </React.Fragment>
     );
   }
 }
+
 export default Input;
