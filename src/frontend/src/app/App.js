811dbbb5e404940488b824b5b349f2881a7daa14
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-02-27 15:59:36 +0700

    create homepage

--- src/frontend/src/app/App.js
+++ src/frontend/src/app/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       		<Router>
                 <Switch>
                     <Route path="/admin/" component={Admin} />
+                    <Route path="/homepage/" component = {Homepage} />
                     <Route path="/signin-admin/" component={SignInAdmin} />
                     <Route component={NotFound} />
                 </Switch>
