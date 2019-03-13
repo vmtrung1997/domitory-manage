f0d9611b1695b851f2f92f864fce498e2e3aa13d
tranthanhtrung <tranthanhtrung1239@gmail.com>
2019-03-13 10:50:29 +0700

    fix get data in app.js

--- src/frontend/src/app/App.js
+++ src/frontend/src/app/App.js
@@ -19,7 +19,7 @@ const checkAuth = () => {
     const secret = JSON.parse(localStorage.getItem('secret'))
     if(secret){
         const decode = jwt_decode(secret.access_token)
-        switch(decode.user.loai)
+        switch(decode.user.userEntity.loai)
         {
             case 'SA':
                 isAdmin = true
