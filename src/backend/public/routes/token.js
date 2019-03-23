6de85a782e5e3feee4a35addeba54acdf2e6fb1b
tranthanhtrung <tranthanhtrung1239@gmail.com>
2019-03-23 14:26:45 +0700

    fix func refresh-token

--- src/backend/public/routes/token.js
+++ src/backend/public/routes/token.js
@@ -1,7 +1,49 @@
 var router = require('express').Router();
+const ObjectId = require('mongoose').mongo.ObjectId;
+
+const User = require('../models/TaiKhoan');
+const Profile = require('../models/Profile')
+const ReToken = require('../models/refreshToken');
+var auth = require('../repos/authRepo');
 
 router.get('/', (req, res) => {
-	res.json({ token:'true' })
+	var reToken = req.headers['x-refresh-token'];
+	ReToken.findOne({ token: reToken }, null, function (err, result) {
+		if (err) {
+			res.status(401).end('end')
+			console.log('==refresh_token: ',err);
+		}
+
+		if (result) {
+			var id = new ObjectId(result.userid);
+			User.findOne({ '_id': id }, function (err, userEntity) {
+				if( err ) {
+					res.status(401).end('end')
+					console.log('==refresh_token: ',err);
+				}
+				if (userEntity) {
+					Profile.findOne({idTaiKhoan: userEntity._id},(err,prof) => {
+						if( prof ){
+							var userObj = {userEntity, profile: prof}
+							var acToken = auth.generateAccessToken(userObj);
+							console.log('==refresh_token: success')
+							res.status(200).json({
+								auth: true,
+								access_token: acToken,
+								refresh_token: reToken
+							})
+						}
+						if( err ){
+							res.status(401).end('end')
+							console.log('==refresh_token: ',err);
+						}
+					})
+				}
+			})
+		} else {
+			res.status(401).end('end')
+		}
+	})
 });
  
 module.exports = router;
