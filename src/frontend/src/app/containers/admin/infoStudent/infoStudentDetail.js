6b66801f7de6714fb3a102b4ef1c1f15091d8d53
MINH-TRUNG-VO\Admin <vmtrung1997@gmail.com>
2019-04-18 14:54:46 +0700

    fix student detail

--- src/frontend/src/app/containers/admin/infoStudent/infoStudentDetail.js
+++ src/frontend/src/app/containers/admin/infoStudent/infoStudentDetail.js
@@ -142,13 +142,13 @@ class InfoStudentDetail extends Component {
       var data = e.target.result;
       var testImg = new Image();
       testImg.src = data;
-      testImg.onload = (e) => {
-        alert('Ảnh đúng')
+      testImg.onload = (event) => {
+        console.log(e.target);
+        this.setState({info: {...this.state.info, img: e.target.result}})
       }
       testImg.onerror = () => {
         alert('Lỗi ảnh')
       }
-			console.log(data);
 		}
   }
   render() {
@@ -173,8 +173,7 @@ class InfoStudentDetail extends Component {
       img,
 
     } = info;
-    console.log('img = ', img)
-    var imgFile = imageFile(img)
+    var imgFile = typeof img === 'string'?img: imageFile(img)
 
     return (
       <div>
@@ -200,10 +199,10 @@ class InfoStudentDetail extends Component {
                     name="file-1[]"
                     id="file-1"
                     className="inputfile inputfile-1"
-                    ref={file => this.uploadFile = file}
+                    ref={file => file?this.uploadFile = file:{files:['']}}
                     onChange={this.onUpload} />
-                  <label htmlFor="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z" /></svg>
-                    {/* <span>Choose a file&hellip;</span> */}
+                  <label htmlFor="file-1">
+                    <span>Tải ảnh</span>
                   </label>
                 </div>
               </Col>
