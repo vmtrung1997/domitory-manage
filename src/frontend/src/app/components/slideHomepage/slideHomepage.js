5a862cf2a0e869e86734c7afcec4b7376313461d
tainguyen197 <tainguyen197.ntt@gmail.com>
2019-05-03 16:36:46 +0700

    add new homepage

--- src/frontend/src/app/components/slideHomepage/slideHomepage.js
+++ src/frontend/src/app/components/slideHomepage/slideHomepage.js
@@ -1,70 +1,119 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-
-const fadeImages = [
-  "/img/skt_1.jpg",
-  "/img/skt_2.jpg",
-  "/img/skt_1.jpg",
-];
+import './styleHomepage.css'
 
 const Slideshow = () => {
   return (
     <React.Fragment>
+
+
       <Carousel>
         <Carousel.Item>
-          <div className="homepage-carousel-img">
-            <img
-           
-              className="d-block w-100"
-              src={fadeImages[0]}
-              alt="First slide"
-            />
-          </div>
-          <Carousel.Caption>
-            <div className = "homepage-carousel-caption">
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* Single Welcome Slide */}
+          <div
+            className="single-welcome-slide bg-img bg-overlay"
+            style={{ backgroundImage: "url(img/post-1.jpg)" }}
+          >
+            {/* Welcome Content */}
+            <div className="welcome-content h-100">
+              <div className="container h-100">
+                <div className="row h-100 align-items-center justify-content-center">
+                  {/* Welcome Text */}
+                  <div className="col-12 col-md-9 col-lg-6">
+                    <div className="welcome-text text-center">
+                      <h2 data-animation="fadeInUpBig" data-delay="100ms">
+                        Designed For Music, Engineered to Last
+                      </h2>
+                      <h5 data-animation="fadeInUpBig" data-delay="400ms">
+                        31st Dec - Night out party....Don't miss it
+                      </h5>
+                      <a
+                        href="#"
+                        className="btn razo-btn btn-2"
+                        data-animation="fadeInUpBig"
+                        data-delay="700ms"
+                      >
+                        Book Now
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <div className="homepage-carousel-img">
-
-            <img
-          
-              className="d-block w-100"
-              src={fadeImages[1]}
-              alt="First slide"
-            />
-          </div>
-
-          <Carousel.Caption>
-          <div className = "homepage-carousel-caption">
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* Single Welcome Slide */}
+          <div
+            className="single-welcome-slide bg-img bg-overlay"
+            style={{ backgroundImage: "url(img/post-1.jpg)" }}
+          >
+            {/* Welcome Content */}
+            <div className="welcome-content h-100">
+              <div className="container h-100">
+                <div className="row h-100 align-items-center justify-content-center">
+                  {/* Welcome Text */}
+                  <div className="col-12 col-md-9 col-lg-6">
+                    <div className="welcome-text text-center">
+                      <h2 data-animation="fadeInUpBig" data-delay="100ms">
+                        Designed For Music, Engineered to Last
+                      </h2>
+                      <h5 data-animation="fadeInUpBig" data-delay="400ms">
+                        31st Dec - Night out party....Don't miss it
+                      </h5>
+                      <a
+                        href="#"
+                        className="btn razo-btn btn-2"
+                        data-animation="fadeInUpBig"
+                        data-delay="700ms"
+                      >
+                        Book Now
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
+        
         <Carousel.Item>
-          <div className="homepage-carousel-img">
-            <img
-              className="d-block w-100"
-              src={fadeImages[0]}
-              alt="First slide"
-            />
-          </div>
-
-          <Carousel.Caption>
-          <div className = "homepage-carousel-caption">
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+          {/* Single Welcome Slide */}
+          <div
+            className="single-welcome-slide bg-img bg-overlay"
+            style={{ backgroundImage: "url(img/post-1.jpg)" }}
+          >
+            {/* Welcome Content */}
+            <div className="welcome-content h-100">
+              <div className="container h-100">
+                <div className="row h-100 align-items-center justify-content-center">
+                  {/* Welcome Text */}
+                  <div className="col-12 col-md-9 col-lg-6">
+                    <div className="welcome-text text-center">
+                      <h2 data-animation="fadeInUpBig" data-delay="100ms">
+                        Designed For Music, Engineered to Last
+                      </h2>
+                      <h5 data-animation="fadeInUpBig" data-delay="400ms">
+                        31st Dec - Night out party....Don't miss it
+                      </h5>
+                      <a
+                        href="#"
+                        className="btn razo-btn btn-2"
+                        data-animation="fadeInUpBig"
+                        data-delay="700ms"
+                      >
+                        Book Now
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
+        
       </Carousel>
-      ;
+
     </React.Fragment>
   );
 };
