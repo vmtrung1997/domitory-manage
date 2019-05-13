ba3c8c4dfffad2a2e9a5d09d1200d5a024e8cafa
Thao Tran <thaophuongtran@chotot.vn>
2019-05-13 13:30:35 +0700

    !merge master into this branch

---
+++ src/frontend/src/app/containers/admin/registered/registeredTable.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Table } from 'react-bootstrap'
+
+class RegisteredTable extends Component{
+	render(){
+		const table = ''
+		return(
+			<Table bordered hover responsive size="sm" className="table-activity">
+				<thead className="title-table">
+					<tr style={{textAlign: 'center'}}>
+						<th>STT</th>
+						<th>MSSV</th>
+						<th>Họ và tên</th>
+						<th>Trường</th>
+						<th>Phòng</th>
+						<th>Điểm rèn luyện</th>
+						<th>Xác nhận</th>
+					</tr>
+				</thead>
+				<tbody>
+					{table}
+				</tbody>
+			</Table>
+		)
+	}
+}
+export default RegisteredTable
