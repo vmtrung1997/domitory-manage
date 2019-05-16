8c09d601e6e75ec8c5cc043e7e8daba4201cbf8d
tranthanhtrung <tranthanhtrung1239@gmail.com>
2019-05-16 22:15:14 +0700

    change

--- src/frontend/src/app/containers/admin/registered/registeredTable.js
+++ src/frontend/src/app/containers/admin/registered/registeredTable.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
 
+import Checkbox from './../../../components/checkbox/checkbox'
+
 class RegisteredTable extends Component{
+	static defaultProps = {
+		data: []
+	}
 	render(){
-		const table = ''
+		const table = this.props.data.map( (row, index) => {
+			return (
+				<tr key={index} >
+					<td>{index + 1}</td>
+					{ row.idProfile ?
+						<>
+						<td>{row.idProfile.MSSV}</td>
+						<td>{row.idProfile.hoTen}</td>
+						<td style={{textAlign: 'center'}}>{row.idProfile.idPhong ? row.idProfile.idPhong.tenPhong : '---' }</td>
+						<td style={{textAlign: 'center'}}>{row.idProfile.hoTen}</td>
+						</>
+					:
+						<>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						</>
+					}
+					<td className={'text-center'} >
+						<Checkbox 
+							style={{ display: 'flex', justifyContent: 'center' }} 
+							check={row.isAccept} 
+						/>
+					</td>
+				</tr>
+			)
+		})
+		console.log(this.props.data)
 		return(
 			<Table bordered hover responsive size="sm" className="table-activity">
 				<thead className="title-table">
@@ -11,9 +44,8 @@ class RegisteredTable extends Component{
 						<th>STT</th>
 						<th>MSSV</th>
 						<th>Họ và tên</th>
-						<th>Trường</th>
 						<th>Phòng</th>
-						<th>Điểm rèn luyện</th>
+						<th>Điểm hoạt động</th>
 						<th>Xác nhận</th>
 					</tr>
 				</thead>
