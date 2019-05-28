fc8a786a1f164c070edc06adb5f18c5c577a2879
MINH-TRUNG-VO\Admin <vmtrung1997@gmail.com>
2019-05-28 16:48:49 +0700

    add build folder

---
+++ src/frontend/build/precache-manifest.6e4952bd7ba041e91285b7de6106ed1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f2d3a19dc179e0f86ca8335e83f1177",
+    "url": "/static/media/logo_HCMUS.3f2d3a19.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6be6337e6f889fe63fc6",
+    "url": "/static/js/main.f29b6fef.chunk.js"
+  },
+  {
+    "revision": "a8274386b77b27c34cbb",
+    "url": "/static/js/2.1ca18460.chunk.js"
+  },
+  {
+    "revision": "6be6337e6f889fe63fc6",
+    "url": "/static/css/main.649ba53c.chunk.css"
+  },
+  {
+    "revision": "a8274386b77b27c34cbb",
+    "url": "/static/css/2.3851ee81.chunk.css"
+  },
+  {
+    "revision": "e237d38001ec9c0166328eb07bad0130",
+    "url": "/index.html"
+  }
+];
