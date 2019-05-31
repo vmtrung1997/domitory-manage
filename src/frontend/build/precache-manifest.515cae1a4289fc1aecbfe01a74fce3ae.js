275096c617a1a54466989dd9e19732edba8ae4f6
MINH-TRUNG-VO\Admin <vmtrung1997@gmail.com>
2019-05-31 13:52:58 +0700

    update

---
+++ src/frontend/build/precache-manifest.515cae1a4289fc1aecbfe01a74fce3ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f2d3a19dc179e0f86ca8335e83f1177",
+    "url": "/static/media/logo_HCMUS.3f2d3a19.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da4dc1decbe365fa1228",
+    "url": "/static/js/main.998df708.chunk.js"
+  },
+  {
+    "revision": "283edd4b9622420e3dad",
+    "url": "/static/js/2.dddbc1a6.chunk.js"
+  },
+  {
+    "revision": "da4dc1decbe365fa1228",
+    "url": "/static/css/main.b3867aa2.chunk.css"
+  },
+  {
+    "revision": "283edd4b9622420e3dad",
+    "url": "/static/css/2.38804119.chunk.css"
+  },
+  {
+    "revision": "43556bdbdac5349efac2965bc45d14ba",
+    "url": "/index.html"
+  }
+];
--- src/frontend/build/service-worker.js
+++ src/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f50aad1e2210d27eb4b16f665b55632d.js"
+  "/precache-manifest.515cae1a4289fc1aecbfe01a74fce3ae.js"
 );
 
 workbox.clientsClaim();
