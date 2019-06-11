2ae276c196509c764a27a11c23370f4ef20c294b
MINH-TRUNG-VO\Admin <vmtrung1997@gmail.com>
2019-06-11 10:37:48 +0700

    fix

---
+++ src/frontend/build/precache-manifest.c092865d44987e3f56e46b528b55d059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f2d3a19dc179e0f86ca8335e83f1177",
+    "url": "/static/media/logo_HCMUS.3f2d3a19.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d06991b599192200b62",
+    "url": "/static/js/main.ca05dabd.chunk.js"
+  },
+  {
+    "revision": "b5d834090960273ed1a1",
+    "url": "/static/js/2.ca91675a.chunk.js"
+  },
+  {
+    "revision": "4d06991b599192200b62",
+    "url": "/static/css/main.b2f61d32.chunk.css"
+  },
+  {
+    "revision": "b5d834090960273ed1a1",
+    "url": "/static/css/2.38804119.chunk.css"
+  },
+  {
+    "revision": "1b8f3067d0b70228cf6ccf5e6442901c",
+    "url": "/index.html"
+  }
+];
